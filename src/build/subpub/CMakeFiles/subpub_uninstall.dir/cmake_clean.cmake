824ea6b1d283638c03155296da1bb19013b12c63
Muchamad Fauzan Atha Artawan <donennone@gmail.com>
2026-02-24 09:46:21 +0700

    Delete src/build directory

--- src/build/subpub/CMakeFiles/subpub_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/subpub_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/subpub_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
